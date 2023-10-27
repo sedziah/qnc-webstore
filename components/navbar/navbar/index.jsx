0bb51966d438e14717998d7d08938c6f9dbddcac
Solomon <sedziah1@gmail.com>
2023-10-27 10:35:42 +0000

    update: Update desktop link component styles

--- components/navbar/navbar/index.jsx
+++ components/navbar/navbar/index.jsx
@@ -3,6 +3,7 @@ import logo from "../assets/images/logo.png";
 import cart from "../assets/icons/cart.svg";
 import styles from "../styles/Navbar.module.css";
 import hamburger from "../assets/icons/hamburger.png";
+import Link from "next/link";
 
 function Navbar() {
   return (
@@ -16,24 +17,40 @@ function Navbar() {
         <div className={styles.desktopNavItems}>
           <ul>
             <div className={styles.nav_links}>
-              <li>featured</li>
-              <li>deals</li>
-              <li>categories</li>
-              <li>
-                <Image src={cart} alt="QnC Logo" width={25} />
-              </li>
+              <Link href="#" className={styles.links}>
+                <li>featured</li>
+              </Link>
+              <Link href="#" className={styles.links}>
+                <li>deals</li>
+              </Link>
+              <Link href="#" className={styles.links}>
+                <li>categories</li>
+              </Link>
+              <Link href="#" className={styles.links}>
+                <li>
+                  <Image src={cart} alt="QnC Logo" width={25} />
+                </li>
+              </Link>
             </div>
             <div className={styles.nav_buttons}>
-              <li className={styles.signin_button}>Sign in</li>
-              <li className={styles.signup_button}>Sign up</li>
+              <Link href="#" className={styles.links}>
+                <li className={styles.signin_button}>Sign in</li>
+              </Link>
+              <Link href="#" className={styles.links}>
+                <li className={styles.signup_button}>Sign up</li>
+              </Link>
             </div>
           </ul>
         </div>
 
-        {/* Mobile Navigation Menu */}
+        {/* Tablet and Smaller Desktop Navigation Menu */}
         <div className={styles.tabletNavItems}>
           <Image src={hamburger} alt="QnC Logo" width={25} />
-          
+        </div>
+
+        {/* Mobile Navigation Menu */}
+        <div className={styles.mobileNavItems}>
+          <Image src={hamburger} alt="QnC Logo" width={25} />
         </div>
       </div>
     </nav>
