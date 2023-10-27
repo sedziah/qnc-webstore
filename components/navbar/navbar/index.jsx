3f56184209b15a58773d2c4fe107b8d3a0a2156f
Solomon <sedziah1@gmail.com>
2023-10-27 09:43:44 +0000

    update: Add mobile and tablet navigation

--- components/navbar/navbar/index.jsx
+++ components/navbar/navbar/index.jsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import logo from "../assets/images/logo.png";
-import search from "../assets/icons/cart.svg";
+import cart from "../assets/icons/cart.svg";
 import styles from "../styles/Navbar.module.css";
+import hamburger from "../assets/icons/hamburger.png";
 
 function Navbar() {
   return (
@@ -10,17 +11,29 @@ function Navbar() {
         <div>
           <Image src={logo} alt="QnC Logo" width={121} height={51.5} />
         </div>
-        <div>
-          <ul className={styles.nav_links}>
-            <li>featured</li>
-            <li>deals</li>
-            <li>categories</li>
-            <li>
-              <Image src={search} alt="QnC Logo" width={20} height={20} />
-            </li>
-            <li className={styles.nav_buttons}>Sign in</li>
-            <li className={styles.nav_buttons}>Sign up</li>
+
+        {/* Desktop Navigation Menu */}
+        <div className={styles.desktopNavItems}>
+          <ul>
+            <div className={styles.nav_links}>
+              <li>featured</li>
+              <li>deals</li>
+              <li>categories</li>
+              <li>
+                <Image src={cart} alt="QnC Logo" width={25} />
+              </li>
+            </div>
+            <div className={styles.nav_buttons}>
+              <li className={styles.signin_button}>Sign in</li>
+              <li className={styles.signup_button}>Sign up</li>
+            </div>
           </ul>
+        </div>
+
+        {/* Mobile Navigation Menu */}
+        <div className={styles.tabletNavItems}>
+          <Image src={hamburger} alt="QnC Logo" width={25} />
+          
         </div>
       </div>
     </nav>
