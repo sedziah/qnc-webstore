9622260cdc99b21fbf471a0f77a057495f907010
Solomon <sedziah1@gmail.com>
2023-11-02 12:41:05 +0000

    Add new navbar file and components

--- components/navbar/navbar/index.jsx
+++ components/navbar/navbar/index.jsx
@@ -1,105 +1,101 @@
+// Navbar.jsx
 "use client";
 import { useState } from "react";
-import Image from "next/image";
-import logo from "../assets/images/logo.png";
-import cart from "../assets/icons/cart.svg";
-import styles from "../styles/Navbar.module.css";
 import Link from "next/link";
-import PermIdentityIcon from "@mui/icons-material/PermIdentity";
-import MenuIcon from "@mui/icons-material/Menu";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-import CloseIcon from "@mui/icons-material/Close";
+import styles from "../styles/Navbar.module.css";
 
 function Navbar() {
+  const [isMobileNavOpen, setMobileNavOpen] = useState(false);
+  const [isTabletNavOpen, setTabletNavOpen] = useState(false);
+
+  const toggleMobileNav = () => {
+    setMobileNavOpen(!isMobileNavOpen);
+  };
+
+  const toggleTabletNav = () => {
+    setTabletNavOpen(!isTabletNavOpen);
+  };
+
+  const closeOverlay = () => {
+    setMobileNavOpen(false);
+    setTabletNavOpen(false);
+  };
+
   return (
-    <div>
-      {/*Mobile Overlay */}
-      <nav className={styles.overlay}>
-        <div className={styles.close_button}>
-          <CloseIcon />
+    <nav>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <Link href="/">Logo</Link>
         </div>
-        <div>
-          <ul>
-            <Link href="#" className={styles.mobileNavLinks}>
-              <li>featured</li>
-            </Link>
-            <Link href="#" className={styles.mobileNavLinks}>
-              <li>deals</li>
-            </Link>
-            <Link href="#" className={styles.mobileNavLinks}>
-              <li>categories</li>
-            </Link>
+
+        {/* Mobile Navigation Menu (Hamburger Menu) */
+        /* Hidden on tablet and desktop views */}
+        <div
+          className={`${styles.mobileNav} ${
+            isMobileNavOpen ? styles.active : ""
+          }`}
+          onClick={toggleMobileNav}
+        >
+          ☰
+        </div>
+
+        {/* Tablet Navigation Menu (Hamburger Menu) */
+        /* Hidden on desktop view */}
+        <div
+          className={`${styles.tabletNav} ${
+            isTabletNavOpen ? styles.active : ""
+          }`}
+          onClick={toggleTabletNav}
+        >
+          ☰
+        </div>
+
+        {/* Desktop Navigation Links */
+        /* Shown on desktop view */}
+        <ul
+          className={`${styles.navLinks} ${
+            isMobileNavOpen || isTabletNavOpen ? styles.hidden : ""
+          }`}
+        >
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/services">Services</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* Navigation Links Overlay (Mobile and Tablet) */
+      /* Shown when the respective Hamburger Menu is open */}
+      {(isMobileNavOpen || isTabletNavOpen) && (
+        <div className={styles.overlay}>
+          <div className={styles.closeButton} onClick={closeOverlay}>
+            &#10005; {/* Close button (X) */}
+          </div>
+          <ul className={styles.navLinks}>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/services">Services</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
           </ul>
         </div>
-      </nav>
-
-      {/* Desktop Navigation */}
-      <nav className={styles.navbar}>
-        <div className={styles.nav_items}>
-          <div>
-            <Link href="/">
-              <Image src={logo} alt="QnC Logo" width={121} height={51.5} />
-            </Link>
-          </div>
-
-          {/* Desktop Navigation Menu */}
-          <div className={styles.desktopNavItems}>
-            <ul>
-              <div className={styles.nav_links}>
-                <Link href="#" className={styles.links}>
-                  <li>featured</li>
-                </Link>
-                <Link href="#" className={styles.links}>
-                  <li>deals</li>
-                </Link>
-                <Link href="#" className={styles.links}>
-                  <li>categories</li>
-                </Link>
-                <Link href="#" className={styles.links}>
-                  <li>
-                    <Image src={cart} alt="QnC Logo" width={25} />
-                  </li>
-                </Link>
-              </div>
-              <div className={styles.nav_buttons}>
-                <Link href="#" className={styles.links}>
-                  <li className={styles.signin_button}>Sign in</li>
-                </Link>
-                <Link href="#" className={styles.links}>
-                  <li className={styles.signup_button}>Sign up</li>
-                </Link>
-              </div>
-            </ul>
-          </div>
-
-          {/* Tablet and Smaller Desktop Navigation Menu */}
-          <div className={styles.tabletNavItems}>
-            <Link href="#" className={styles.links}>
-              <PermIdentityIcon />
-            </Link>
-            <Link href="#" className={styles.links}>
-              <AddShoppingCartIcon />
-            </Link>
-            <Link href="#" className={styles.links}>
-              <MenuIcon />
-            </Link>
-          </div>
-
-          {/* Mobile Navigation Menu */}
-          <div className={styles.mobileNavItems}>
-            <Link href="#" className={styles.links}>
-              <PermIdentityIcon />
-            </Link>
-            <Link href="#" className={styles.links}>
-              <AddShoppingCartIcon />
-            </Link>
-            <Link href="#" className={styles.links}>
-              <MenuIcon />
-            </Link>
-          </div>
-        </div>
-      </nav>
-    </div>
+      )}
+    </nav>
   );
 }
 
