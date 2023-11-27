431b86ae326444ad11aeff7d17ab9555bfd7655c
Solomon <sedziah1@gmail.com>
2023-11-27 16:06:50 +0000

    File refrence errors fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Hero from '@/components/hero';
-import SearchBar from '@/components/searchbar';
+import Hero from '../components/hero';
+import SearchBar from '../components/searchbar';
 import React from 'react'
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import styles from "./page.module.css"
