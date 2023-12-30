b2f8af80ea31534e5bae5b2041fc548de821e2e8
Solomon Edziah <68519058+sedziah@users.noreply.github.com>
2023-12-30 00:18:26 +0000

    Add images and text to hero section

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div>
       <SearchBar />
-      {/* <Hero /> */}
+      <Hero />
     </div>
   );
 }
---
+++ components/hero/index.tsx
@@ -0,0 +1,27 @@
+// components/hero/index.tsx
+
+import Link from "next/link";
+import styles from "./Hero.module.css";
+import Image from "next/image";
+import Stack from "@mui/material/Stack";
+
+function Hero() {
+  //Hero Images
+  const hero_accesories = "/images/hero/hero_accessories.png";
+  const hero_computers = "/images/hero/hero_computers.png";
+  const hero_appliances = "/images/hero/hero_appliances.png";
+  const hero_mobile_phone = "/images/hero/hero_mobile_phone.png";
+
+  return (
+    <div className={styles.heroSection}>
+      <div className={`${styles.imageContainer} ${styles.mobilePhones}`}>
+        Mobile Phones
+      </div>
+      <div className={`${styles.imageContainer} ${styles.computers}`}>Computer</div>
+      <div className={`${styles.imageContainer} ${styles.accessories}`}>Accessories</div>
+      <div>D</div>
+    </div>
+  );
+}
+
+export default Hero;
