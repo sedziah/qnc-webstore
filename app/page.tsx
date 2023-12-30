e832dfd252072af715d484627b71a5326c3f4480
Solomon Edziah <68519058+sedziah@users.noreply.github.com>
2023-12-30 23:36:45 +0000

    Add banner

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero from '../components/hero';
 import SearchBar from '../components/searchbar';
 import DealsHeader from '../components/deals/index';
 import Carousel from "../components/imageSlider/index";
+import Banner from "../components/banner/index";
 import React from 'react'
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       <Hero />
       <DealsHeader />
       <Carousel />
+      <Banner src="/images/banner/qnc_banner_1.png" alt="banner"/>
     </div>
   );
 }
