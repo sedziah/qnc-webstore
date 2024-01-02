aa692fed72788f29bf6106128b4605e66afc5fe7
Solomon Edziah <68519058+sedziah@users.noreply.github.com>
2024-01-02 22:12:46 +0000

    Remove banner

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Hero from '../components/hero';
 import SearchBar from '../components/searchbar';
 import DealsHeader from '../components/deals/index';
 import Carousel from "../components/imageSlider/index";
-import Banner from "../components/banner/index";
 import React from 'react'
 
 export default function Home() {
@@ -14,7 +13,6 @@ export default function Home() {
       <Hero />
       <DealsHeader />
       <Carousel />
-      <Banner src="/images/banner/qnc_banner_1.png" alt="banner"/>
     </div>
   );
 }
