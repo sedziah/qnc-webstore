e0a84f191884471b5a8366e936d630c78953e196
Solomon Edziah <68519058+sedziah@users.noreply.github.com>
2024-01-25 23:26:57 +0000

    signup page updated

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -5,7 +5,8 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../auth/contexts/AuthContext"; // Adjust path as needed
 import { apiService } from "../../services/apiService";
-import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
 
 const Page = () => {
   const router = useRouter();
@@ -78,86 +79,162 @@ const Page = () => {
       alert("Verification email resent! Please check your inbox.");
     } catch (err) {
       // setError(err.message);
-       if (err instanceof Error) {
-         setError(err.message); // Now TypeScript knows err is an Error object
-       } else {
-         setError("An unexpected error occurred"); // Generic error message for unknown types
-       }
+      if (err instanceof Error) {
+        setError(err.message); // Now TypeScript knows err is an Error object
+      } else {
+        setError("An unexpected error occurred"); // Generic error message for unknown types
+      }
     }
   };
 
   return (
     <div className={styles.container}>
-      <div className={styles.formContainer}>
-        <form onSubmit={handleSubmit}>
-          <h1>Sign Up </h1>
-          <br></br>
-          <input
-            type="text"
-            name="firstName"
-            className={styles.inputField}
-            placeholder="First Name"
-            onChange={handleChange}
-            value={formData.firstName}
-            required
-          />
-          <input
-            type="text"
-            name="lastName"
-            className={styles.inputField}
-            placeholder="Last Name"
-            onChange={handleChange}
-            value={formData.lastName}
-            required
-          />
-          <input
-            type="email"
-            name="email"
-            className={styles.inputField}
-            placeholder="Email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            className={styles.inputField}
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-          <input
-            type="password"
-            name="confirmPassword"
-            className={styles.inputField}
-            placeholder="Repeat Password"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            required
-          />
-          <button className={styles.submitButton}>Continue</button>
+      <Head>
+        <title>Login Page</title>
+      </Head>
+
+      <div className={styles.formSide}>
+        <form className={styles.loginForm} onSubmit={handleSubmit}>
+          <div className={styles.inputGroup}>
+            <label htmlFor="firstname">First Name *</label>
+            <input
+              type="text"
+              name="firstName"
+              onChange={handleChange}
+              value={formData.firstName}
+              required
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="lastname">Last Name *</label>
+            <input
+              type="text"
+              name="lastName"
+              onChange={handleChange}
+              value={formData.lastName}
+              required
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="email">Email *</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="password">Password *</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="password">Confirm Password *</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <button type="submit" className={styles.loginButton}>
+            Sign Up
+          </button>
+
           {error && <p className={styles.error}>{error}</p>}
         </form>
-        <Link href="/signin">Sign In</Link>
-
-        {/* <div className={styles.separator}>
-          <span className={styles.separatorLine}>or</span>
-        </div>
-
-        <button className={`${styles.submitButton} ${styles.socialButton}`}>
-          Continue with Facebook
-        </button>
-        <button className={`${styles.submitButton} ${styles.socialButton}`}>
-          Continue with Google
-        </button>
-        <button className={`${styles.submitButton} ${styles.socialButton}`}>
-          Continue with Apple
-        </button> */}
+      </div>
+      <div className={styles.imageSide}>
+        <Image
+          src="/images/signup.png" // Replace with the path to your actual image
+          alt="Workspace Background"
+          layout="fill"
+          objectFit="cover"
+          priority // This will prioritize loading of the image
+        />
       </div>
     </div>
   );
 };
 
 export default Page;
+
+//<div className={styles.container}>
+//   <div className={styles.formContainer}>
+//     <form onSubmit={handleSubmit}>
+//       <h1>Sign Up </h1>
+//       <br></br>
+//       <input
+//         type="text"
+//         name="firstName"
+//         className={styles.inputField}
+//         placeholder="First Name"
+//         onChange={handleChange}
+//         value={formData.firstName}
+//         required
+//       />
+//       <input
+//         type="text"
+//         name="lastName"
+//         className={styles.inputField}
+//         placeholder="Last Name"
+//         onChange={handleChange}
+//         value={formData.lastName}
+//         required
+//       />
+//       <input
+//         type="email"
+//         name="email"
+//         className={styles.inputField}
+//         placeholder="Email"
+//         value={formData.email}
+//         onChange={handleChange}
+//         required
+//       />
+//       <input
+//         type="password"
+//         name="password"
+//         className={styles.inputField}
+//         placeholder="Password"
+//         value={formData.password}
+//         onChange={handleChange}
+//         required
+//       />
+//       <input
+//         type="password"
+//         name="confirmPassword"
+//         className={styles.inputField}
+//         placeholder="Repeat Password"
+//         value={formData.confirmPassword}
+//         onChange={handleChange}
+//         required
+//       />
+//       <button className={styles.submitButton}>Continue</button>
+//       {error && <p className={styles.error}>{error}</p>}
+//     </form>
+//     <Link href="/signin">Sign In</Link>
+
+//     {/* <div className={styles.separator}>
+//       <span className={styles.separatorLine}>or</span>
+//     </div>
+
+//     <button className={`${styles.submitButton} ${styles.socialButton}`}>
+//       Continue with Facebook
+//     </button>
+//     <button className={`${styles.submitButton} ${styles.socialButton}`}>
+//       Continue with Google
+//     </button>
+//     <button className={`${styles.submitButton} ${styles.socialButton}`}>
+//       Continue with Apple
+//     </button> */}
+//   </div>
+// </div>
