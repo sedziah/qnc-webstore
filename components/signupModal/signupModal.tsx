5d5dd74296f0c24f95fa96dc69e3f7094aef15a6
Solomon Edziah <68519058+sedziah@users.noreply.github.com>
2024-01-26 00:09:23 +0000

    modal added

---
+++ components/signupModal/signupModal.tsx
@@ -0,0 +1,31 @@
+// components/SignUpSuccessModal.tsx
+import React from "react";
+import styles from "./modal.module.css"; // Ensure this is the correct path to your CSS module
+
+interface SignUpSuccessModalProps {
+  onSignInClick: () => void;
+  onClose: () => void; // Added for closing the modal
+}
+
+const SignUpSuccessModal: React.FC<SignUpSuccessModalProps> = ({
+  onSignInClick,
+  onClose,
+}) => {
+  return (
+    <div className={styles.modalOverlay} onClick={onClose}>
+      <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+        {" "}
+        {/* Prevents click inside the modal from closing it */}
+        <p>
+          Account successfully created. Check your email to activate your
+          account.
+        </p>
+        <button onClick={onSignInClick} className={styles.signInButton}>
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpSuccessModal;
