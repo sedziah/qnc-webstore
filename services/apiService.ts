fe39c117cec0dddd2f4e2f1c1cb901c0ccd651f9
Solomon Edziah <68519058+sedziah@users.noreply.github.com>
2024-04-01 12:24:48 +0000

    Update apiservice to capture smartphone catgory

--- services/apiService.ts
+++ services/apiService.ts
@@ -1,7 +1,7 @@
 // services/apiService.ts
 
-// const API_BASE_URL = "http://167.172.52.195";
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "http://167.172.52.195";
+// const API_BASE_URL = "http://127.0.0.1:8000";
 
 interface Variant {
   id: string;
@@ -273,7 +273,7 @@ export const apiService = {
   },
 
   getMobilePhones: async (): Promise<TransformedProduct[]> => {
-    return await apiService.getProductsByCategory("smart-phones");
+    return await apiService.getProductsByCategory("smartphones");
   },
 
   getComputers: async (): Promise<TransformedProduct[]> => {
