86748de43ba336cc71a807a2e98739fc07444c2d
Solomon Edziah <68519058+sedziah@users.noreply.github.com>
2024-04-07 19:29:53 +0000

    Add no products message to computer and mobile phone pages

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -40,15 +40,17 @@ function Footer() {
           world that demands the best. We believe that quality isn't just a
           goal—it's our promise to you.
         </p>
-        <p>+233 54 1234 567</p>
-        <p>Banana Street, East Legon</p>
+        <br/>
+        <p>+233 20 7597 903</p>
+        <p>17 Hill Street, Accra New Town</p>
       </div>
       <div className={styles.linksSection}>
         <h2>Quick Links</h2>
+        <br/>
         <Link href="/about">About Us</Link>
-        <Link href="/store-credits">Store Credits</Link>
-        <Link href="/appliances">Appliances</Link>
-        <Link href="/computers">Computers</Link>
+        <Link href="/store-credits">Q&C Credit</Link>
+        <Link href="/products/appliances">Appliances</Link>
+        <Link href="/products/computers">Computers</Link>
       </div>
       <div className={styles.dealsSection}>
         <h2>Receive daily deals</h2>
